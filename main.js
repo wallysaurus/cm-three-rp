c64be0a80c584c17236478d81515b6d4efb1f61d
WallyTube <wolfy9917@gmail.com>
2022-10-18 17:36:05 -0500

    Model loader fix (take 2)

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 import './style.css';
-import './resources/models/R-computer-two-glow.gltf';
-import './resources/models/R-computer-two.gltf';
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment';
-import { SelectiveBloomEffect } from 'postprocessing';
-
-//
-//    INIT
-//
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+//import { SelectiveBloomEffect } from 'postprocessing';
 
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 const camera = new THREE.PerspectiveCamera(4, innerWidth / innerHeight, 1, 1000);
+const controls = new OrbitControls( camera, renderer.domElement );
+const cameraOrthoHelper = new THREE.CameraHelper( camera );
 
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0 ).texture;
+scene.add( cameraOrthoHelper );
 renderer.setClearColor( 0x000000, 0 );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -24,30 +23,21 @@ camera.position.x = -40;
 camera.position.y = 27;
 camera.position.z = 235;
 
-// Better Lighting
-const pmremGenerator = new THREE.PMREMGenerator( renderer );
-scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
-
-// Orbit Controls
-const controls = new OrbitControls( camera, renderer.domElement );
-
-//
-//    MODEL LOADER
-//
-
+var block = [];
 const loader = new GLTFLoader();
-
-loader.load('./resources/models/R-computer-two-glow.gltf', function (gltf) {
+loader.load('./resources/models/R-computer-two.gltf', function (gltf) {
 		scene.add( gltf.scene );
+		block.push( gltf.scene );
 	},
 	function ( xhr ) {}, // loading
 	function ( error ) { console.log( 'An error happened' ); } // error loading
 );
 loader.load('./resources/models/R-computer-two-glow.gltf', function (gltf) {
-	  scene.add( gltf.scene );
-  },
-  function ( xhr ) {}, // loading
-  function ( error ) { console.log( 'An error happened' ); } // error loading
+	scene.add( gltf.scene );
+	block.push( gltf.scene );
+},
+function ( xhr ) {}, // loading
+function ( error ) { console.log( 'An error happened' ); } // error loading
 );
 
 window.addEventListener('resize', () => {
@@ -57,7 +47,9 @@ window.addEventListener('resize', () => {
 })
 
 renderer.setAnimationLoop(() => {
+	//block.forEach(b => b.rotation.y += 0.02);
+
 	console.log(camera.position);
 	controls.update();
 	renderer.render(scene, camera);
-});
+})
